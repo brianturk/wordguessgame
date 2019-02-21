a4e582e3d8d7c3619ab1501719f81fc49de95b39
Brian Turk <bturksub@gmail.com>
2019-02-20 21:00:42 -0800

    added duplicate letter message

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,75 +19,77 @@ document.getElementById('gametype').innerHTML += "&nbsp" + gameType;
 var blankHangman = "";
 var guessedLetters = "";
 var blanks = 0;
-for (var x = 0;x < st.length;x++) {
-    if (st.substring(x,x+1) === " ") {
-        blankHangman = blankHangman + " ";
-        blanks++;
-    } else {
-        blankHangman = blankHangman + "-";
-    }
+for (var x = 0; x < st.length; x++) {
+	if (st.substring(x, x + 1) === " ") {
+		blankHangman = blankHangman + " ";
+		blanks++;
+	} else {
+		blankHangman = blankHangman + "-";
+	}
 };
 document.getElementById('puzzle').innerHTML = blankHangman;
 document.getElementById('guessesleft').innerHTML = guessesLeft;
 
-document.onkeyup = function (event){
-    var key = event.key || event.keyCode;
-    key = key.toLowerCase();
-    var match = false;
+document.onkeyup = function (event) {
+	var key = event.key || event.keyCode;
+	key = key.toLowerCase();
+	var match = false;
 
 	if (firsttime) {
 		firsttime = false;
-		
+
 	}
 
 
-    if ((key.search(/[^A-Za-z]/g)) && (guessedLetters.search(key) === -1) && (!(gameover)) && (key.length === 1) ) {    //only take letters as input and it is a new letter
-        
-        guessedLetters += key;
+	if ((key.search(/[^A-Za-z]/g)) && (!(gameover)) && (key.length === 1)) {    //only take letters as input and it is a new letter
 
-        //Check to see if letter is in the word
-        for (x = 0; x < st.length; x++){
-            if (st.substring(x,x+1) === key) {   //Is match
-                if (x === 0) {
-                    blankHangman = key + blankHangman.substring(1,blankHangman.length);
-                } else {
-                    blankHangman = blankHangman.substring(0,x) + key + blankHangman.substring(x+1,blankHangman.length);
-                }
-                match = true;
-            }
-        }
+		if (guessedLetters.search(key) === -1) {   //not a previously guessed letter
+			guessedLetters += key;
+			//Check to see if letter is in the word
+			for (x = 0; x < st.length; x++) {
+				if (st.substring(x, x + 1) === key) {   //Is match
+					if (x === 0) {
+						blankHangman = key + blankHangman.substring(1, blankHangman.length);
+					} else {
+						blankHangman = blankHangman.substring(0, x) + key + blankHangman.substring(x + 1, blankHangman.length);
+					}
+					match = true;
+				}
+			}
 
-		document.getElementById('puzzle').innerHTML = blankHangman;
-		
-		if (match) { //check for word done
-			if (blankHangman === st) {
-				gameover = true;
-				document.getElementById('result').innerHTML = "You got it!";
-				document.getElementById('bodybg').style.background = "rgb(110, 230, 110)";
-			}else {
-				document.getElementById('result').innerHTML = "Good job! Keep going...";
+			document.getElementById('puzzle').innerHTML = blankHangman;
+
+			if (match) { //check for word done
+				if (blankHangman === st) {
+					gameover = true;
+					document.getElementById('result').innerHTML = "You got it!";
+					document.getElementById('bodybg').style.background = "rgb(110, 230, 110)";
+				} else {
+					document.getElementById('result').innerHTML = "Good job! Keep going...";
+				}
+			} else { //add letter to wrong letter list and decrement guesses
+				document.getElementById('wrong').innerHTML = key + "&nbsp" + document.getElementById('wrong').innerHTML;
+				guessesLeft--;
+				document.getElementById('guessesleft').innerHTML = guessesLeft;
+				if (guessesLeft === 0) {
+					gameover = true;
+					document.getElementById('result').innerHTML = "You lost :(";
+					document.getElementById('bodybg').style.background = "rgb(247, 60, 60)";
+					document.getElementById('word').innerHTML = "Word: " + st;
+				} else {
+					document.getElementById('result').innerHTML = "Sorry, that's wrong. Keep going...";
+				}
 			}
-		} else { //add letter to wrong letter list and decrement guesses
-			document.getElementById('wrong').innerHTML = key + "&nbsp" + document.getElementById('wrong').innerHTML;
-			guessesLeft--;
-			document.getElementById('guessesleft').innerHTML = guessesLeft;
-			if (guessesLeft === 0) {
-				gameover = true;
-				document.getElementById('result').innerHTML = "You lost :(";
-				document.getElementById('bodybg').style.background = "rgb(247, 60, 60)";
-				document.getElementById('word').innerHTML = "Word: " + st;
-			} else {
-				document.getElementById('result').innerHTML = "Sorry, that's wrong. Keep going...";
+
+			if (gameover) {
+				document.getElementById('definition').innerHTML = "Definition: " + sport_terms[stNum][2];
 			}
+		} else {
+			document.getElementById('result').innerHTML = "Letter guessed already. Keep going...";
 		}
 
-		if (gameover) {
-			document.getElementById('definition').innerHTML = "Definition: " + sport_terms[stNum][2];
-		}
-		
-		
-        
-    }
+
+	}
 };
 
 console.log(st);
